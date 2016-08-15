1ab474f2e74164ad1486a2e427edc1032fc93646
Junya Ogasawara <junya-ogasawara@cookpad.jp>
2016-08-15 10:44:49 +0900

    Testing data binding.

--- app/src/main/java/com/cookpad/android/marketapp/MainActivity.java
+++ app/src/main/java/com/cookpad/android/marketapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.cookpad.android.marketapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.cookpad.android.marketapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.textView.setText("hogehoge");
+        binding.marketText.setText("marketText");
     }
 }
